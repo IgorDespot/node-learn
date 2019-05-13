f850ab0066a75a2ef856bf1cdd307f1ab6ab6f82
Tamara Dragovic <Tamara.Dragovic@eng.it>
2019-05-13 12:50:03 +0200

    DB new connection with MongoAtlas

--- util/db.js
+++ util/db.js
@@ -2,18 +2,10 @@ const mongoose = require('mongoose');
 var database = require('../config').database;
 //var logger = require('./logger');
 
-const url = 'mongodb://' + database.user + ':' + database.password + '@' + database.host + ':' + database.port + '/' + database.name; 
-
+//const url = 'mongodb://' + database.user + ':' + database.password + '@' + database.host + ':' + database.port + '/' + database.name; 
+const url = 'mongodb+srv://' + database.user + ':' + database.password + '@nodeproject-tu8wo.mongodb.net/test?retryWrites=true'
 
 var connection = mongoose.connect(url , { useNewUrlParser: true});
 
-if(connection){
-    console.log('Connection is created!');
-    //logger.info('Connection is created!');
-
-}else{
-    console.log('You have problem with connection!');
-    //logger.info('You have problem with connection!');
-};
 
 module.exports = connection;
