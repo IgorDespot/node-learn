c8157dd54da82116d0d620b9918720931393573b
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-13 16:28:31 +0200

    route set for /users

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.use(express.urlencoded({ extended: false }));
 
 
 //app.use('/admin', adminRouter);
-//app.use('/users', usersRouter);
+app.use('/users', usersRouter);
 
-app.use((req,res,next) => {
-    res.status(404).send('Error 404 Page not found!');
-});
+// app.use((req,res,next) => {
+//     res.status(404).send('Error 404 Page not found!');
+// });
 
 module.exports = app;
