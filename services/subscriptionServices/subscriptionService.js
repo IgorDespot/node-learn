19ef3128b656e362333bded71b44a5f2f798f57e
StefanAtanaskovic <zeljko.jelicic@eng.it>
2019-05-13 12:49:08 +0200

    dodao neke funcionalnosti u subscription api

--- services/subscriptionServices/subscriptionService.js
+++ services/subscriptionServices/subscriptionService.js
@@ -1,5 +1,20 @@
-function sendDataToApi(isSubscribed, data) {
+let rp = require('request-promise');
+
+function sendDataToApi(isSubscribed, uri, data) {
     if (isSubscribed) {
-        
+        let options = {
+            method: 'POST',
+            uri: uri,
+            body: {
+                data: data
+            },
+            json: true
+        };
+
+        rp(options)
+        .then( console.log('It works'))
+        .catch( console.log('It didnt work'));
     }
 }
+
+module.exports = sendDataToApi;
