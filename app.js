9e08f300fad9956afc7284c88e72e3d31a059fee
Tamara Dragovic <Tamara.Dragovic@eng.it>
2019-05-13 12:17:18 +0200

    updated Server

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.use('/admin', adminRouter);
-app.use('/users', usersRouter);
+//app.use('/admin', adminRouter);
+//app.use('/users', usersRouter);
 
 app.use((req,res,next) => {
     res.status(404).send('Error 404 Page not found!');
