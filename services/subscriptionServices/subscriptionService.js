1f81be65f556628e65a970f1afa7b1c0b95cb4af
StefanAtanaskovic <zeljko.jelicic@eng.it>
2019-05-13 15:38:49 +0200

    Changed subscription service

--- services/subscriptionServices/subscriptionService.js
+++ services/subscriptionServices/subscriptionService.js
@@ -1,19 +1,25 @@
 let rp = require('request-promise');
 
-function sendDataToApi(isSubscribed, uri, data) {
-    if (isSubscribed) {
-        let options = {
-            method: 'POST',
-            uri: uri,
-            body: {
-                data: data
-            },
-            json: true
-        };
+function sendDataToApi(doSub, isSubscribed, uri, data) {
+    if (doSub) {
+        if (isSubscribed) {
+            let options = {
+                method: 'POST',
+                uri: uri,
+                body: {
+                    data: data
+                },
+                json: true
+            };
 
-        rp(options)
-        .then( resp => console.log('It works'))
-        .catch( err => console.log('It didnt work'));
+            rp(options)
+                .then(x => console.log('It works'))
+                .catch(x => console.log('It didnt work'));
+        } else {
+            return {
+                errorMsg: "If you wish to subscribe your data to the thir party service change your account setting to allow subscription"
+            }
+        }
     }
 }
 
